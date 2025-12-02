373b8dacb05d37df2079bab5cb1aabfaabccac19
Ali <alikaya4275@gmail.com>
2025-12-02 16:36:00 +0300

    AppUser sınıfı ile özelleştirme

---
+++ Context/EmailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NotikaIdentityEmail.Entities;
+
+namespace NotikaIdentityEmail.Context
+{
+    public class EmailContext : IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=NotikaEmailDb;Trusted_Connection=True;trust server certificate=true");
+        }
+    }
+}
