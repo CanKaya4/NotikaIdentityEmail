41645648da1e2f4f586b3218f2947267207e16b3
Ali <alikaya4275@gmail.com>
2025-12-02 16:53:08 +0300

    Register Backend Kodları

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NotikaIdentityEmail.Entities;
+using NotikaIdentityEmail.Models;
+
+namespace NotikaIdentityEmail.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(RegisterUserViewModel registerUserViewModel)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = registerUserViewModel.Name,
+                Email = registerUserViewModel.Email,
+                Surname = registerUserViewModel.Surname,
+                UserName = registerUserViewModel.UserName
+            };
+            var result = await _userManager.CreateAsync(appUser, registerUserViewModel.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("UserLogin", "LoginController");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View();
+        }
+    }
+}
